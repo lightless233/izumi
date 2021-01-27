867f2691cea272d588ab0540cd8c33a7b9c55113
lightless <lightless@foxmail.com>
2021-01-27 11:51:45 +0800

    add ryuo

--- src/main/kotlin/me/lightless/izumi/plugin/timer/impl/Ryuo.kt
+++ src/main/kotlin/me/lightless/izumi/plugin/timer/impl/Ryuo.kt
@@ -1,11 +1,14 @@
 package me.lightless.izumi.plugin.timer.impl
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import me.lightless.izumi.ApplicationContext
 import me.lightless.izumi.dao.ChatMessage
 import me.lightless.izumi.dao.ChatMessageDAO
 import me.lightless.izumi.plugin.timer.ITimer
 import net.mamoe.mirai.Bot
+import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.buildMessageChain
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
@@ -44,16 +47,16 @@ class Ryuo : ITimer {
 
             // 每天早上 9 点 30 分，发送龙王数据
             if (h == 9 && m == 30) {
-//                TODO()
                 val today = LocalDate.now(DateTimeZone.forOffsetHours(8))
                 val yesterday = today.minusDays(1)
                 val ryuoMap = mutableMapOf<Long, MutableMap<Long, Long>>()
+                val nicknameMap = mutableMapOf<Long, MutableMap<Long, String>>()
 
                 // 从数据库里查出来昨天的全部信息
                 val messageList = transaction {
                     ChatMessageDAO.find {
-                        ChatMessage.createdTime less today and
-                                (ChatMessage.createdTime greaterEq yesterday)
+                        ChatMessage.createdTime less today.toDateTimeAtStartOfDay() and
+                                (ChatMessage.createdTime greaterEq yesterday.toDateTimeAtStartOfDay())
                     }.toList()
                 }
 
@@ -61,24 +64,50 @@ class Ryuo : ITimer {
                 for (msgDAO in messageList) {
                     if (msgDAO.groupId !in ryuoMap.keys) {
                         ryuoMap[msgDAO.groupId] = mutableMapOf()
+                        nicknameMap[msgDAO.groupId] = mutableMapOf()
                     }
-                    val innerMap = ryuoMap[msgDAO.groupId] ?: continue
-                    if (msgDAO.qq !in innerMap.keys) {
-                        innerMap[msgDAO.qq] = 0
+                    val ryuoInnerMap = ryuoMap[msgDAO.groupId] ?: continue
+                    val nicknameInnerMap = nicknameMap[msgDAO.groupId] ?: continue
+
+                    if (msgDAO.qq !in ryuoInnerMap.keys) {
+                        ryuoInnerMap[msgDAO.qq] = 0
                     } else {
-                        innerMap[msgDAO.qq] = innerMap[msgDAO.qq] as Long + 1
+                        ryuoInnerMap[msgDAO.qq] = ryuoInnerMap[msgDAO.qq] as Long + 1
                     }
+
+                    nicknameInnerMap[msgDAO.qq] = msgDAO.nickname
                 }
                 this.logger.debug("ryuoMap: $ryuoMap")
 
                 // 发消息
-//                for (allowedGroupId in allowedGroups) {
-//                    val innerMap = ryuoMap.get(allowedGroupId) ?: continue
-//
-//                }
-//                var message = "[龙王通知] 恭喜 xxx 成为今天的龙王，快来给大家表演个喷水吧！\n"
+                for (allowedGroupId in allowedGroups) {
+                    val ryuoInnerMap = ryuoMap[allowedGroupId] ?: continue
+                    val nicknameInnerMap = nicknameMap[allowedGroupId] ?: continue
+                    val sortedInnerMap = ryuoInnerMap.entries
+                        .sortedByDescending { it.value }.associateBy({ it.key }, { it.value })
+                    this.logger.debug("sortedInnerMap: $sortedInnerMap")
 
+                    var ryuoId = 0L
+                    var yesterdayMessage = "昨日摸鱼：\n"
+                    sortedInnerMap.forEach { (t, u) ->
+                        if (ryuoId == 0L) {
+                            ryuoId = t
+                        }
+                        yesterdayMessage += "${nicknameInnerMap[t]}($t) -> $u\n"
+                    }
 
+                    val fullMessage = buildMessageChain {
+                        add("[龙王] 恭喜 ")
+                        add(At(ryuoId))
+                        add(" 成为今天的龙王，快来给大家表演个喷水吧！\n")
+                        add(yesterdayMessage)
+                    }
+
+                    bot.launch {
+                        bot.getGroup(allowedGroupId)?.sendMessage(fullMessage)
+                    }
+
+                }
                 // 多 sleep 5 秒，防止同一分钟内发两次消息
                 delay(65 * 1000)
             } else {
